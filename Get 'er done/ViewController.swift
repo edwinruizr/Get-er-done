8b260bd42d2b870cea2a9f7048cef0b7dbccef14
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-12-24 13:57:35 -0800

    Initial Commit

---
+++ Get 'er done/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Get 'er done
+//
+//  Created by Edwin Ruiz on 12/24/17.
+//  Copyright © 2017 Edwin Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
