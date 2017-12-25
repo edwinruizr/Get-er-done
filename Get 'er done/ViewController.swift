a8b7ea29464b3df6dfccbe8742e9b3ce7b289e15
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-12-24 20:40:42 -0800

    create a table view with initial hard coded data

--- Get 'er done/ViewController.swift
+++ Get 'er done/ViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    let itemArray = ["Learn", "Stay consistent", "be awesome"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK - Tableview datasource methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 }
 
