35d82e275ddad997cbecb3437c73758d6d107000
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-12-25 12:07:43 -0800

    added + button to nav bar and allowed the input of new items. reloads table view after new item entered to display it.

--- Get 'er done/ViewController.swift
+++ Get 'er done/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    let itemArray = ["Learn", "Stay consistent", "be awesome"]
+    var itemArray = ["Learn", "Stay consistent", "be awesome"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,5 +40,33 @@ class ViewController: UITableViewController {
         // don't highlight the row selected 
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            // what happens after the user hits the add item button
+            //print(textField.text!)
+            
+            // add new item to our array
+            self.itemArray.append(textField.text!)
+            
+            // now we need to reload our table view
+            self.tableView.reloadData()
+            
+            //print("Sucess")
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
