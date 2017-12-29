459fa6f774b6290534a9a61dd4895ed7f88de004
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-12-29 10:21:24 -0800

    able to save to realm

---
+++ Get er done/AppDelegate.swift
@@ -0,0 +1,71 @@
+//
+//  AppDelegate.swift
+//  Get 'er done
+//
+//  Created by Edwin Ruiz on 12/24/17.
+//  Copyright © 2017 Edwin Ruiz. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import RealmSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        //print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
+
+        // to see the realm file for simulator
+        //print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+            let realm = try Realm()
+        } catch  {
+            print("Error initializing Realm object, \(error)")
+        }
+        
+        return true
+    }
+
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+        self.saveContext()
+    }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+
+}
+
