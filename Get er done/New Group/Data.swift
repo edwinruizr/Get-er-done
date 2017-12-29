df0ee28fb6eb04622ed4341c87ddb8ed959fd90e
Edwin Ruiz <edwinruizr1992@gmail.com>
2017-12-28 21:57:22 -0800

    added RealmSwift CocoaPods and was able to write data to Realm

---
+++ Get er done/New Group/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  Get er done
+//
+//  Created by Edwin Ruiz on 12/28/17.
+//  Copyright © 2017 Edwin Ruiz. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+    
+}
